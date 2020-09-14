cdc29e8fc25512a5a49197f81ed18349ae5c874c
Brett Monzel <brett@MWA272.attlocal.net>
2020-09-13 17:20:04 -0700

    added customizable features

--- includes/youtubesubs-scripts.php
+++ includes/youtubesubs-scripts.php
@@ -7,4 +7,9 @@ function yts_add_scripts(){
 
     //Add main JS
     wp_enqueue_script('yts-main-script', plugins_url(). '/youtubesubs/js/main.js');
+
+    //Add google/youtube script
+    wp_register_script('google', 'https://apis.google.com/js/platform.js');
+    wp_enqueue_script('google');
 }
+add_action('wp_enqueue_scripts', 'yts_add_scripts');
